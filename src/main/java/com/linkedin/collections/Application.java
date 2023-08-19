b726fbe067f31988885871c20b8bfa417696e555
sdacleofe <103588804+sdacleofe@users.noreply.github.com>
2023-08-19 03:28:18 +0000

    Working with Collection

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -1,18 +1,25 @@
 package com.linkedin.collections;
 
+import java.util.Collection;
+import java.util.List;
+
 public class Application {
 
 	public static void main(String[] args) {
 	
 		Room cambridge = new Room("Cambridge", "Premiere Room", 4, 175.00);
 		Room manchester = new Room("Manchester", "Suite", 5, 250.0);
+		Room picadilly = new Room("Picadilly", "Guest Room", 3, 105.0);
+		Room oxford = new Room("Oxford", "Suite", 3, 125.0);
 
-		double total = getPotentialRevenue(cambridge, manchester);
+		Collection<Room> rooms = List.of(cambridge, manchester, picadilly, oxford);
+		double total = getPotentialRevenue(rooms);
 		System.out.println(total);
 	}
 	
-	private static double getPotentialRevenue(Room room1, Room room2) {
-		return room1.getRate() + room2.getRate();
+	private static double getPotentialRevenue(Collection<Room> rooms) {
+		return rooms.stream()
+					.mapToDouble(r -> r.getRate())
+					.sum();
 	}
-
 }
