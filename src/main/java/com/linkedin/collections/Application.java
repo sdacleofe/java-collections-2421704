d3a8d289ce1f1d9ccffdbaa3f4fa2b410aea4848
sdacleofe <103588804+sdacleofe@users.noreply.github.com>
2023-08-20 08:21:40 +0000

    ArrayList

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -1,5 +1,6 @@
 package com.linkedin.collections;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
@@ -11,6 +12,24 @@ public class Application {
 		Guest sonia = new Guest("Sonia", "Doe", true);
 		Guest siri = new Guest("Siri", "Doe", true);
 
+		List<Guest> checkinList = new ArrayList<>(100);
+		checkinList.add(john);
+		checkinList.add(maria);
+
+		print(checkinList);
+
+		checkinList.add(0, sonia);
+		print(checkinList);
+
+		checkinList.get(2).setLoyaltyProgramMember(true);;
+
+		checkinList.addAll(1, List.of(maria, siri));
+		print(checkinList);
+
+		checkinList.remove(checkinList.size() -1);
+		print(checkinList);
+
+		System.out.println(checkinList.indexOf(john));
 
 	}
 
