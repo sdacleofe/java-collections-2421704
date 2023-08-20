168e07b9c242756f822ba92b05279114ae8e9105
sdacleofe <103588804+sdacleofe@users.noreply.github.com>
2023-08-20 07:54:24 +0000

    TreeSet

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -2,12 +2,22 @@ package com.linkedin.collections;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.NavigableSet;
+import java.util.TreeSet;
 
 public class Application {
 
 	public static void main(String[] args) {
 		
 		List<Integer> numbers = Arrays.asList(500, 1500, 2500, 1000, 3000, 2000);
+
+		NavigableSet<Integer> numberTree = new TreeSet<>(numbers);
+
+		System.out.println(numberTree.lower(750));
+
+		// numberTree.subSet(1750, 2750)
+		// 	.stream()
+		// 	.forEach(System.out::println);
 		
 	}
 }
